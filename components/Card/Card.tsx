5857769ae26b5ddeb1bf04cdbc2531062cf75b0c
ItsSosha <whoissosha@gmail.com>
2023-07-12 23:29:14 +0200

    feat: add card background prop

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,13 +1,20 @@
 import { PropsWithChildren } from "react";
 import styles from "./Card.module.css";
 
+type BackgroundColors = "primary" | "light-blue";
+
 interface CardProps {
   img: string;
+  background?: BackgroundColors;
 }
 
-export default function Card({ img, children }: PropsWithChildren<CardProps>) {
+export default function Card({
+  img,
+  background = "primary",
+  children,
+}: PropsWithChildren<CardProps>) {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${styles[background]}`}>
       <img className={styles.image} src={img} />
       <div className={styles.body}>{children}</div>
     </div>
