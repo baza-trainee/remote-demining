1dc169c64771fabbf3027ea0fc3bfdf6bcbefbcd
Olga1Chernova <olya53800@gmail.com>
2023-07-12 19:43:19 +0300

    Created section container

---
+++ components/SectionContainer/SectionContainer.jsx
@@ -0,0 +1,50 @@
+import styles from "./sectionContainer.module.css"
+
+interface ContainerProps {
+  title?: string;
+  bgImg?: string;
+  titleColor?: string;
+  children?: React.ReactNode;
+  description?: string;
+  centerTitle?: boolean;
+}
+
+const SectionContainer: React.FC<ContainerProps> = ({
+  title,
+  bgImg,
+  titleColor = "#151515",
+  children,
+  description,
+  centerTitle = false,
+}) => {
+  const containerStyle = {
+    backgroundImage: bgImg ? `url(${bgImg})` : "none",
+  };
+
+  const titleStyle = {
+    color: titleColor,
+  };
+
+  const containerHeaderWrapperClassName = centerTitle
+    ? `${styles.containerHeaderWrapper} ${styles.centered}`
+    : styles.containerHeaderWrapper;
+
+  return (
+    <div className={styles.container} style={containerStyle}>
+      <div className={containerHeaderWrapperClassName}>
+        {title && (
+          <h2 className={styles.title} style={titleStyle}>
+            {title}
+          </h2>
+        )}
+        {description && <span className={styles.descLine}></span>}
+        {description && <p className={styles.desc}>{description}</p>}
+      </div>
+      {children}
+    </div>
+  );
+};
+
+SectionContainer.displayName = "SectionContainer";
+
+export default SectionContainer;
