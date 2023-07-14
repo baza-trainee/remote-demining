4239e307ab6e08d0b2d0673fbbb04521d0ce731f
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-07-14 21:19:58 +0300

    Added drone styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,9 @@ import Button from "@/components/Button/Button";
 import Drone from "./_Drone/Drone";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Drone />
+    </main>
+  );
 }
