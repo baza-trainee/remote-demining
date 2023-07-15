6e5fb9491564ffcd5292ee8d1256b307ec5e1ce7
ViraMesha <wira.mescha@gmail.com>
2023-07-15 10:52:36 +0300

    feat: add Tasks component, icons and bg img

---
+++ components/Tasks/Tasks.tsx
@@ -0,0 +1,18 @@
+import tasksData from "./tasksData";
+import TasksList from "./TasksList/TasksList";
+import SectionContainer from "../SectionContainer/SectionContainer";
+
+const Tasks: React.FC = () => {
+  return (
+    <SectionContainer
+      title="Основні завдання"
+      bgImg="/images/task-bg.jpg"
+      titleColor="var(--task-text-color)"
+      centerTitle
+    >
+      <TasksList items={tasksData} />
+    </SectionContainer>
+  );
+};
+
+export default Tasks;
