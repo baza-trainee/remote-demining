b56cd524c2008c1986fb832ac14876920ee2ab1b
ViraMesha <wira.mescha@gmail.com>
2023-07-16 17:23:28 +0300

    feat: delete Button from Home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import Button from "@/components/Button/Button";
-import Input from "@/components/Input/Input";
-
 export default function Home() {
-  return (
-    <main>
-      <Button donateBtn>Підтримати</Button>
-    </main>
-  );
+  return <main></main>;
 }
