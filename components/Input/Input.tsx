ebbb863e65e597c8fb749da263b96091ead1ab41
Anton <zaharchenko3500@gmail.com>
2023-07-16 18:43:17 +0300

    refactor: styles and add wrapper for Input

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -27,7 +27,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     };
 
     return (
-      <>
+      <div>
         {label && <label className={styles.label}>{label}</label>}
         <input
           ref={ref}
@@ -35,7 +35,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           className={`${styles[size]} ${styles.input}`}
           style={InputStyle}
         />
-      </>
+      </div>
     );
   }
 );
