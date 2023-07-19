b6a31e6e0d00670e10769a4708db86a27efad5c0
ViraMesha <wira.mescha@gmail.com>
2023-07-19 09:44:17 +0300

    add styles for mobile

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -10,7 +10,18 @@ const Projects = () => {
       title="Переваги проєкту "
       description="Ми також надаємо корисні поради щодо вибору дрона, використання його можливостей та правил безпеки. Наша команда експертів завжди готова надати вам консультацію та допомогти з будь-якими питаннями, пов'язаними з дронами."
     >
-      <CardList
+      <ul className={styles.list}>
+        {projectsData.map(({ id, img, title, text }) => (
+          <li key={id} className={styles.card}>
+            <img src={img} className={styles.image} />
+            <div className={styles.body}>
+              <h3 className={styles.title}>{title}</h3>
+              <p className={styles.text}>{text}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {/* <CardList
         perView={3}
         items={projectsData.map((item) => (
           <Card key={item.id} img={item.img} background="light-blue">
@@ -18,7 +29,7 @@ const Projects = () => {
             <p className={styles.text}>{item.text}</p>
           </Card>
         ))}
-      />
+      /> */}
     </SectionContainer>
   );
 };
