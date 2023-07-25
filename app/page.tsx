8ffcde5c3ba3719df040ee67950d90548f1c4dd7
ViraMesha <wira.mescha@gmail.com>
2023-07-26 00:18:04 +0300

    create Partnership component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-
 import AboutSection from "@/components/AboutSection/AboutSection";
+import Partnership from "@/components/Partnership/Partnership";
 import Projects from "@/components/Projects/Projects";
 import Tasks from "@/components/Tasks/Tasks";
 
 export default function Home() {
   return (
     <main>
-      <AboutSection/>
+      <AboutSection />
       <Tasks />
       <Projects />
+      <Partnership />
     </main>
   );
 }
---
+++ components/Partnership/Partnership.tsx
@@ -0,0 +1,14 @@
+import SectionContainer from "../SectionContainer/SectionContainer";
+
+import styles from "./Partnership.module.css";
+
+const Partnership = () => {
+  return (
+    <SectionContainer
+      title="Партнерські організації"
+      description="Організації, з якими ми постійно співпрацюємо"
+    ></SectionContainer>
+  );
+};
+
+export default Partnership;
