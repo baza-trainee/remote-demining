8c28c39636918504b2b54048ab7542cb8f662427
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-07-25 13:50:37 +0300

    Added MediaHook, added NewsSlider

--- components/NewsSection/NewsItem/NewsItem.tsx
+++ components/NewsSection/NewsItem/NewsItem.tsx
@@ -1,12 +1,36 @@
+import Card from "@/components/Card/Card";
 import CustomLink from "@/components/CustomLink/CustomLink";
+import styles from "./NewsItem.module.css";
 
 interface NewsItem {
+  id: number;
   text: string;
-  img: any;
+  img: string;
+  title: string;
 }
 
-const NewsItem: React.FC<NewsItem> = ({ img, text }) => {
-  return <li></li>;
+const NewsItem: React.FC<NewsItem> = ({ id, img, text, title }) => {
+  return (
+    <div key={id} className={styles.item}>
+      <Card img={img}>
+        <div className={styles.content}>
+          <div>
+            <p className={styles.title}>{title}</p>
+            <p className={styles.text}>{text}</p>
+          </div>
+          <div>
+            <CustomLink href="/" isFullWidth>
+              Детальніше
+            </CustomLink>
+            <div className={styles.date}>
+              <span className={styles.decor}></span>
+              <p>06 липня 2023</p>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default NewsItem;
--- components/NewsSection/NewsSection.tsx
+++ components/NewsSection/NewsSection.tsx
@@ -1,10 +1,34 @@
+"use client";
+import { useMyMedia } from "@/hooks/useMedia";
+import { useEffect, useState } from "react";
 import SectionContainer from "../SectionContainer/SectionContainer";
-import NewsList from "./NewsList/NewsList";
+import Slider from "../Slider/Slider";
+import NewsItem from "./NewsItem/NewsItem";
+import newsData, { News } from "./newsData";
 
 const NewsSection: React.FC = () => {
+  const [perPage, setPerPage] = useState<number>(1);
+  const { isMobile, isTablet, isDesktop } = useMyMedia();
+  useEffect(() => {
+    isMobile && setPerPage(1);
+    isTablet && setPerPage(2);
+    isDesktop && setPerPage(3);
+  }, [isMobile, isTablet, isDesktop]);
+
   return (
-    <SectionContainer>
-      <NewsList items={[]} />
+    <SectionContainer
+      title="Наші новини"
+      description="Ми також надаємо корисні поради щодо вибору дрона, використання його можливостей та правил безпеки. Наша команда експертів завжди готова надати вам консультацію та допомогти з будь-якими питаннями, пов'язаними з дронами."
+    >
+      <Slider
+        slidesPerPage={perPage}
+        slides={newsData.map((elem: News) => (
+          <NewsItem {...elem} />
+        ))}
+        dots
+        infinite
+        dotsPlacement="inner"
+      />
     </SectionContainer>
   );
 };
