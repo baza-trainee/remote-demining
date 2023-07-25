a833653b6749b7d560476dd3a605575f23e82621
betadin21 <132145328+betadin21@users.noreply.github.com>
2023-07-25 17:08:28 +0300

    response footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,54 +1,70 @@
 import styles from "./Footer.module.css";
-import logo from "./icons/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
-import tel from "./icons/tel.svg";
-import mail from "./icons/mail.svg";
-import map from "./icons/map.svg"
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.wrapper}>
-        <Link href="" className={styles.logo}>
-          <Image src={logo} alt="logo" />
+        <Link href={"#"} className={styles.logo}>
+          <Image height={149} width={117} src="/icons/logo.svg" alt="logo" />
         </Link>
-        <div className={`${styles.menu} ${styles.category}`}>
-          <h4>Меню</h4>
-          <Link href="">
-            <p>Про нас</p>
-          </Link>
-          <Link href="">
-            <p>Наукова діяльність</p>
-          </Link>
-          <Link href="">
-            <p>Наукова діяльність</p>
-          </Link>
-          <Link href="">
-            <p>Контакти</p>
-          </Link>
-        </div>
-        <div className={`${styles.web__rights} ${styles.category}`}>
-          <h4>Правила</h4>
-          <Link href="">
-            <p>Політика конфіденційності</p>
-          </Link>
-          <Link href="">
-            <p>Правила користування</p>
-          </Link>
-        </div>
-        <div className={`${styles.contacts} ${styles.category}`}>
-          <h4>Контакти</h4>
-          <Link href="">
-            <p className={styles.contacts__list}><Image src={tel} alt="tel" /> <span>+38(044)2095302</span> </p>
-          </Link>
-          <Link href="">
-            <p className={styles.contacts__list}> <Image src={mail} alt="mail" /> <span>2021snp@ukr.net</span> </p>
-          </Link>
-          <Link href="">
-            <p className={styles.contacts__list}><Image src={map} alt="map" /> <span>вул. Олеся Гончара 55 б, Київ, Україна 01601</span></p>
-          </Link>
-        </div>
+        <ul className={`${styles.menu} ${styles.category}`}>
+          <li className={styles.category__name}>Меню</li>
+          <li>
+            <Link href="">Про нас</Link>
+          </li>
+          <li>
+            <Link href="">Наукова діяльність</Link>
+          </li>
+          <li>
+            <Link href="">Дистанційне розмінування</Link>
+          </li>
+          <li>
+            <Link href="">Контакти</Link>
+          </li>
+        </ul>
+        <ul className={`${styles.web__rights} ${styles.category}`}>
+          <li className={styles.category__name}>Правила</li>
+          <li>
+            <Link target={"_blank"} href="">
+              Політика конфіденційності
+            </Link>
+          </li>
+          <li>
+            <Link target={"_blank"} href="">
+              Правила користування сайтом
+            </Link>
+          </li>
+          <li>
+            <Link target={"_blank"} href="">
+              Статут
+            </Link>
+          </li>
+        </ul>
+        <ul className={`${styles.contacts} ${styles.category}`}>
+          <li className={styles.category__name}>Контакти</li>
+          <li className={styles.contacts__list}>
+            <Image width={22} height={22} src="/icons/tel.svg" alt="tel" />{" "}
+            <span>+38(044)2095302</span>
+          </li>
+          <li>
+            <Link
+              className={styles.contacts__list}
+              href="mailto:2021snp@ukr.net"
+            >
+              <Image width={22} height={22} src="/icons/mail.svg" alt="mail" />
+              <span>2021snp@ukr.net</span>
+            </Link>
+          </li>
+          <li className={styles.contacts__list}>
+            <Image width={22} height={22} src="/icons/map.svg" alt="map" />
+            <span>вул. Олеся Гончара 55 б, Київ, Україна 01601</span>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.baza}>
+        Розробка BazaTraineeUkraine 2023 Всі права захищені
       </div>
     </footer>
   );
