ceb26f42ec468f71d8b792da151d47b22e991c28
olexanderf <alex1990sfreem@gmail.com>
2023-07-26 22:49:06 +0300

    upd delete coment

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-
 import AboutSection from "@/components/AboutSection/AboutSection";
-import HeroSocrat from "@/components/HeroSocrat/HeroSocrat";
 import Projects from "@/components/Projects/Projects";
 import Tasks from "@/components/Tasks/Tasks";
 
 export default function Home() {
   return (
     <main>
-      {/* <AboutSection/>
+      <AboutSection />
       <Tasks />
-      <Projects /> */}
-      <HeroSocrat />
+      <Projects />
     </main>
   );
 }
