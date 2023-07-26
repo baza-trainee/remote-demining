5023e777861304f3fe636bf5fd23f49a2c18140e
ViraMesha <wira.mescha@gmail.com>
2023-07-26 17:12:50 +0300

    update Button styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-
 import AboutSection from "@/components/AboutSection/AboutSection";
+import Button from "@/components/Button/Button";
 import Projects from "@/components/Projects/Projects";
 import Tasks from "@/components/Tasks/Tasks";
 
 export default function Home() {
   return (
     <main>
-      <AboutSection/>
+      <Button>Support</Button>
+      <AboutSection />
       <Tasks />
       <Projects />
     </main>
