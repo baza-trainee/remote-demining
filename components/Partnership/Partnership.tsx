302ae2ad73aa4136820f7be6467212b150dddcfe
ViraMesha <wira.mescha@gmail.com>
2023-07-28 13:11:12 +0300

    add Slider

--- components/Partnership/Partnership.tsx
+++ components/Partnership/Partnership.tsx
@@ -1,13 +1,42 @@
-import SectionContainer from "../SectionContainer/SectionContainer";
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
-import styles from "./Partnership.module.css";
+import { useMyMedia } from "@/hooks/useMedia";
+
+import SectionContainer from "../SectionContainer/SectionContainer";
+import Slider from "../Slider/Slider";
+
+import partnersData, { Partner } from "./partnersData";
 
 const Partnership = () => {
+  const [perPage, setPerPage] = useState<number>(1);
+  const { isMobile, isTablet, isDesktop } = useMyMedia();
+  useEffect(() => {
+    isMobile && setPerPage(1.5);
+    isTablet && setPerPage(3);
+    isDesktop && setPerPage(5.5);
+  }, [isMobile, isTablet, isDesktop]);
+
   return (
     <SectionContainer
       title="Партнерські організації"
       description="Організації, з якими ми постійно співпрацюємо"
-    ></SectionContainer>
+    >
+      <Slider
+        slidesPerPage={perPage}
+        slides={partnersData.map((el: Partner) => (
+          <Image
+            key={el.id}
+            src={el.img}
+            alt="partner"
+            width={213}
+            height={140}
+          />
+        ))}
+        infinite
+      />
+    </SectionContainer>
   );
 };
 
