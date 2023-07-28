3c84e9095663fa6306b038c353a913af8597b147
ViraMesha <wira.mescha@gmail.com>
2023-07-28 15:59:06 +0300

    add styles for btn_link

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import AboutSection from "@/components/AboutSection/AboutSection";
-import Button from "@/components/Button/Button";
 import Projects from "@/components/Projects/Projects";
 import Tasks from "@/components/Tasks/Tasks";
 
 export default function Home() {
   return (
     <main>
-      <Button>Support</Button>
       <AboutSection />
       <Tasks />
       <Projects />
