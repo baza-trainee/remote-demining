edc15238498b78625346d81a37617d0ddbf1bdb6
olexanderf <alex1990sfreem@gmail.com>
2023-07-31 18:52:38 +0300

    add to socrat page components

--- app/socrat/page.tsx
+++ app/socrat/page.tsx
@@ -1,9 +1,13 @@
 import HeroSocrat from "@/components/HeroSocrat/HeroSocrat"
+import SocratInfo from "@/components/SocratInfo/SocratInfo"
+import WhatWeOffer from "@/components/WhatWeOffer/WhatWeOffer"
 
 const SocratPage = () => {
   return (
     <div>
       <HeroSocrat />
+      <SocratInfo />
+      <WhatWeOffer />
     </div>
   )
 }
