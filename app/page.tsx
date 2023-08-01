fe867ff10d5f92e6b8155e8b0d702bf6c9ec70a4
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-01 20:29:02 +0300

    changet to use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-import { redirect } from "next/navigation";
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Home() {
-  redirect("/client");
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push("/client");
+  }, []);
+  return <p></p>;
 }
