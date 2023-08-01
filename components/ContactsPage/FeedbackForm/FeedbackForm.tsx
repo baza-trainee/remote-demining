526caddfab851947ed51c0aa0996655e356d88ed
ViraMesha <wira.mescha@gmail.com>
2023-08-01 17:09:50 +0300

    upd wrapper's styles in FBF

--- components/ContactsPage/FeedbackForm/FeedbackForm.tsx
+++ components/ContactsPage/FeedbackForm/FeedbackForm.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useWindowSize } from "usehooks-ts";
+
 import Button from "@/components/Button/Button";
 import Input from "@/components/Input/Input";
 import TextArea from "@/components/TextArea/TextArea";
@@ -5,6 +9,8 @@ import TextArea from "@/components/TextArea/TextArea";
 import styles from "./FeedbackForm.module.css";
 
 const FeedbackForm = () => {
+  const { width } = useWindowSize();
+
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.heading}>Зворотній зв`язок</h2>
@@ -20,20 +26,41 @@ const FeedbackForm = () => {
           backgroundCl="secondary"
           errorMessage="Ім'я є обов'язковим полем"
         />
-        <Input
-          size="full"
-          type="tel"
-          label="Телефон*"
-          backgroundCl="secondary"
-          errorMessage="Телефон є обов'язковим полем"
-        />
-        <Input
-          size="full"
-          type="email"
-          label="Email*"
-          backgroundCl="secondary"
-          errorMessage="Email є обов'язковим полем"
-        />
+        {width >= 320 && width < 768 ? (
+          <div>
+            <Input
+              size="full"
+              type="tel"
+              label="Телефон*"
+              backgroundCl="secondary"
+              errorMessage="Телефон є обов'язковим полем"
+            />
+            <Input
+              size="full"
+              type="email"
+              label="Email*"
+              backgroundCl="secondary"
+              errorMessage="Email є обов'язковим полем"
+            />
+          </div>
+        ) : (
+          <div className={styles.inputWrapper}>
+            <Input
+              size="small"
+              type="tel"
+              label="Телефон*"
+              backgroundCl="secondary"
+              errorMessage="Телефон є обов'язковим полем"
+            />
+            <Input
+              size="small"
+              type="email"
+              label="Email*"
+              backgroundCl="secondary"
+              errorMessage="Email є обов'язковим полем"
+            />
+          </div>
+        )}
         <TextArea
           label="Повідомлення*"
           size="full"
