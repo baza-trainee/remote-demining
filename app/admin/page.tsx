1544bb3e4c59d0b44654379386472f94685034b5
olexanderf <alex1990sfreem@gmail.com>
2023-08-01 17:17:42 +0300

    adapt header and add admin routing

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <h1>Admin Panel</h1>
+  )
+}
+
+export default page
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 import btn_close_burger from "@/public/images/icons/header/btn_close_burger.svg";
@@ -20,17 +21,18 @@ const Header = () => {
   const toggleMenu = () => {
     setIsOpenMenu(!isOpenMenu);
   };
+  const pathname = usePathname();
   const contentBox = `${styles.box} ${isOpenMenu ? styles.bottom_border : ""}`;
   return (
     <header className={styles.header}>
       <Container>
         <div className={contentBox}>
           <HeaderLogo />
-          <HeaderMenu />
+          {pathname !== '/admin' && <HeaderMenu />}
           <div className={styles.box}>
             <LanguageMenu />
             <Button>Підтримати</Button>
-            <button className={styles.btn_menu} onClick={toggleMenu}>
+            {pathname !== '/admin' &&  <button className={styles.btn_menu} onClick={toggleMenu}>
               <Image
                 className={styles.burger_icon}
                 src={isOpenMenu ? btn_close_burger : burger_menu}
@@ -38,10 +40,10 @@ const Header = () => {
                 width={27}
                 height={30}
               ></Image>
-            </button>
+            </button>}
           </div>
         </div>
-        {isOpenMenu && (
+        {isOpenMenu && pathname !== '/admin' && (
           <MobileMenu isOpenMenu={isOpenMenu} toggleMenu={toggleMenu} />
         )}
       </Container>
