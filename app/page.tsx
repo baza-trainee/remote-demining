d7ab2080be890337c5fc3ec6aa1a567e8618b848
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-01 19:46:28 +0300

    added redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 export default function Home() {
-  // redirect("/client");
-  return <main>main</main>;
+  redirect("/client");
 }
