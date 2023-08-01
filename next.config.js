0354ed5f3a6ca088ea4295947a60db64054c471c
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-01 17:23:14 +0300

    refactor: changed app routes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
   env: {
-    SERVICE_ID: 'service_7ll2pvb',
-    TEMPLATE_ID: 'template_2j1mjx4',
-    PUBLIC_KEY: 'vpKbRCkkOvFZKJUoA',
-    RECAPTCHA_SITE: '6LcC1VwnAAAAAAM8s2T5FyidAYdXPuYT9vByiRyJ',
+    SERVICE_ID: "service_7ll2pvb",
+    TEMPLATE_ID: "template_2j1mjx4",
+    PUBLIC_KEY: "vpKbRCkkOvFZKJUoA",
+    RECAPTCHA_SITE: "6LcC1VwnAAAAAAM8s2T5FyidAYdXPuYT9vByiRyJ",
   },
 };
 
