729e2836c047ab9148a9748a644e4e6a8a9054b2
olexanderf <alex1990sfreem@gmail.com>
2023-08-01 18:10:36 +0300

    add admin layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+
+import Header from "@/components/Header/Header";
+
+import "./globals.css";
+import "./normalize.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const open_sans = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={open_sans.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
