7ee1f0d0f8a63093ee6f48675f27819cca921ad4
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-01 19:05:55 +0300

    Revert "refactor: changed app routes"

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
-import { redirect } from "next/navigation";
+import AboutSection from "@/components/AboutSection/AboutSection";
+import Contact from "@/components/Contact/Contact";
+import NewsSection from "@/components/NewsSection/NewsSection";
+import Partnership from "@/components/Partnership/Partnership";
+import Projects from "@/components/Projects/Projects";
+import Tasks from "@/components/Tasks/Tasks";
+import Team from "@/components/Team/Team";
 
 export default function Home() {
-  redirect("/client");
+  return (
+    <main>
+      <AboutSection />
+      <Tasks />
+      <Projects />
+      <Team />
+      <NewsSection />
+      <Partnership />
+      <Contact />
+    </main>
+  );
 }
