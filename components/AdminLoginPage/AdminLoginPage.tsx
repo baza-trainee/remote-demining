0852b8f1cd81bc53ed38b1b4401b8f4565efdf52
ViraMesha <wira.mescha@gmail.com>
2023-08-03 23:10:26 +0300

    feat: update container

---
+++ components/AdminLoginPage/AdminLoginPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import styles from "./AdminLoginPage.module.css";
+
+const AdminLoginPage: FC = () => {
+  return <div>AdminLoginPage</div>;
+};
+
+export default AdminLoginPage;
