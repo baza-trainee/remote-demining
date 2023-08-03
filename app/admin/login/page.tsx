4d0e6bc13cf85bece6a84303bab47949aca6c055
ViraMesha <wira.mescha@gmail.com>
2023-08-04 00:34:55 +0300

    feat: add layout and styles for AdminLoginPage

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,9 @@
+import AdminLoginPage from "@/components/AdminLoginPage/AdminLoginPage";
+
+export default function Login() {
+  return (
+    <main>
+      <AdminLoginPage />
+    </main>
+  );
+}
--- components/AdminLoginPage/AdminLoginPage.tsx
+++ components/AdminLoginPage/AdminLoginPage.tsx
@@ -1,9 +1,36 @@
 import { FC } from "react";
 
+import AdminWrapper from "../AdminWrapper/AdminWrapper";
+import AutorizationInput from "../AutorizationInput/AutorizationInput";
+import PasswordInputToggle from "../PasswordInputToggle/PasswordInputToggle";
+
 import styles from "./AdminLoginPage.module.css";
+import Link from "next/link";
+import Button from "../Button/Button";
 
 const AdminLoginPage: FC = () => {
-  return <div>AdminLoginPage</div>;
+  return (
+    <section className={styles.section}>
+      <AdminWrapper size="smallWrapper" classname={styles.wrapper}>
+        <h3 className={styles.heading}>Вхід</h3>
+        <form className={styles.form}>
+          <AutorizationInput
+            name="login"
+            label="Логін"
+            type="text"
+            placeholder="Введіть логін"
+          />
+          <PasswordInputToggle />
+        </form>
+        <Link href={"#"} className={styles.link}>
+          Забули пароль?
+        </Link>
+        <Button isFullWidth type="submit">
+          Вхід
+        </Button>
+      </AdminWrapper>
+    </section>
+  );
 };
 
 export default AdminLoginPage;
