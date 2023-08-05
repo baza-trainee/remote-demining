1962b762fb4ffd400968748665e215c730950f80
ViraMesha <wira.mescha@gmail.com>
2023-08-05 18:10:21 +0300

    feat: delete needless code from admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 
-import AdminNavBar from "@/components/AdminNavBar/AdminNavBar";
 import Container from "@/components/Container/Container";
 import Header from "@/components/Header/Header";
-
-import styles from "./layout.module.css";
 
 export const metadata: Metadata = {
   title: "",
