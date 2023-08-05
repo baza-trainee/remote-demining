865d6bd39081b34bb387b3c93c71cece9a91de11
olexanderf <alex1990sfreem@gmail.com>
2023-08-05 21:19:03 +0300

    change content, fix bug container height

--- components/NewsSection/newsData.ts
+++ components/NewsSection/newsData.ts
@@ -1,31 +1,30 @@
-import NewsIMG from "@/public/images/projects/robot-hand.jpg";
+import News_one from "@/public/images/news/news1.png";
+import News_two from "@/public/images/news/news2.png";
+import News_three from "@/public/images/news/news3.png";
 export interface News {
   id: number;
   img: string;
   text: string;
   title: string;
 }
-const tmpText =
-  "Ми також надаємо корисні поради щодо вибору дрона, використання його можливостей та правил безпеки. Наша команда експертів завжди готова надати вам консультацію та допомогти з будь-якими питаннями, пов'язаними з дронами.";
-const tmpTitle = "Title";
 const newsData: News[] = [
   {
     id: 1,
-    img: NewsIMG.src,
-    text: tmpText,
-    title: tmpTitle,
+    img: News_one.src,
+    text: "In a year and a half of conflict, land mines — along with unexploded bombs, artillery shells and other...",
+    title: "Ukraine is now the most mined country. It will take decades to make safe. — The Washington Post.",
   },
   {
     id: 1,
-    img: NewsIMG.src,
-    text: tmpText,
-    title: tmpTitle,
+    img: News_two.src,
+    text: "Захід відбувся 17 травня 2023 року в Києві...",
+    title: "Науковці НАН України представили свої розробки на форумі «Безпека критичної інфраструктури та гуманітарна протимінна діяльність»",
   },
   {
     id: 1,
-    img: NewsIMG.src,
-    text: tmpText,
-    title: tmpTitle,
+    img: News_three.src,
+    text: "Науковці Академії продемонструють свої розр...",
+    title: "Установи НАН України візьмуть участь у форумі «Безпека критичної інфраструктури та гуманітарна протимінна діяльність»",
   },
 ];
 
