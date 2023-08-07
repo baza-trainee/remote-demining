0010d97a0a833b9db60effe9fb5733a25b7a52f9
olexanderf <alex1990sfreem@gmail.com>
2023-08-07 23:13:51 +0300

    cody style fix

--- app/admin/[...forgot-password]/page.tsx
+++ app/admin/[...forgot-password]/page.tsx
@@ -7,13 +7,14 @@ import PasswordReset from "@/components/PasswordRecover/PasswordReset/PasswordRe
 
 const ForgotPasswordPage = () => {
   const pathname = usePathname();
-  const [user_data, setUserData] = useState({id: "", token: ""});
+  const [user_data, setUserData] = useState({ id: "", token: "" });
 
   useEffect(() => {
     const arr = pathname
       .trim()
-      .split("/").filter((el) => el.match(/^(?!.*\b(admin|forgot-password)\b).+$/))
-      setUserData({ id: arr[0], token: arr[1]});
+      .split("/")
+      .filter((el) => el.match(/^(?!.*\b(admin|forgot-password)\b).+$/));
+    setUserData({ id: arr[0], token: arr[1] });
   }, [pathname]);
 
   return (
