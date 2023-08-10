944f1bdcc6a5dcfc4add4c74048e23d64db66509
Olha <107894331+Olga1Chernova@users.noreply.github.com>
2023-08-10 12:38:27 +0300

    Update passwordChange.ts

--- lib/passwordChange.ts
+++ lib/passwordChange.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 
 axios.defaults.baseURL = "https://remote-demining.onrender.com";
 
@@ -21,15 +21,12 @@ const passwordChange = async (
       confirmPassword,
     });
     return data;
-  } catch (error) {
-    // if (error.response) {
-    //   const { status } = error.response;
-    //   if (status === 404) {
-    //     throw new Error("User not found");
-    //   }
-    // }
-    console.log(error);
-    throw new Error("Oh, something went wrong");
+  } catch (error: AxiosError) {
+  if (error.response) {
+    const { status } = error.response;
+    if (status === 404) {
+      throw new Error("User not found");
+    }
   }
 };
 
