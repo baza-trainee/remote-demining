29b458eda018e92890d817cc341e3aea84ec4439
Vira Mesha <99128741+ViraMesha@users.noreply.github.com>
2023-08-10 12:42:42 +0300

    Revert "Update passwordChange.ts"

--- lib/passwordChange.ts
+++ lib/passwordChange.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse, AxiosError } from "axios";
+import axios from "axios";
 
 axios.defaults.baseURL = "https://remote-demining.onrender.com";
 
@@ -21,12 +21,15 @@ const passwordChange = async (
       confirmPassword,
     });
     return data;
-  } catch (error: AxiosError) {
-  if (error.response) {
-    const { status } = error.response;
-    if (status === 404) {
-      throw new Error("User not found");
-    }
+  } catch (error) {
+    // if (error.response) {
+    //   const { status } = error.response;
+    //   if (status === 404) {
+    //     throw new Error("User not found");
+    //   }
+    // }
+    console.log(error);
+    throw new Error("Oh, something went wrong");
   }
 };
 
