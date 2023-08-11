a96bd89188ada74f4946d7c5d558f07a2616a9d2
ViraMesha <wira.mescha@gmail.com>
2023-08-11 18:12:57 +0300

    change bg img for contact form in main and socrat pages

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -6,8 +6,7 @@ import Partnership from "@/components/Partnership/Partnership";
 import Projects from "@/components/Projects/Projects";
 import Tasks from "@/components/Tasks/Tasks";
 import Team from "@/components/Team/Team";
-
-import bgContactsMainPage from "@/public/images/contact/bg.jpg";
+import bgContactsMainPage from "@/public/images/contact/bg-cont-socrat.jpg";
 
 export default function Home() {
   return (
