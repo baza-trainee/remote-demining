531d5cece52a80842f16dd596c2f9e4a182434f5
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-11 17:46:44 +0300

    ref

--- app/client/socrat/page.tsx
+++ app/client/socrat/page.tsx
@@ -1,10 +1,10 @@
-import Contact from '@/components/Contact/Contact';
-import HeroSocrat from '@/components/HeroSocrat/HeroSocrat';
-import SocratInfo from '@/components/SocratInfo/SocratInfo';
-import WhatWeOffer from '@/components/WhatWeOffer/WhatWeOffer';
-import bgContactsSocratPage from '@/public/images/contact/bg-cont-socrat.jpg';
+import Contact from "@/components/Contact/Contact";
+import HeroSocrat from "@/components/HeroSocrat/HeroSocrat";
+import SocratInfo from "@/components/SocratInfo/SocratInfo";
+import WhatWeOffer from "@/components/WhatWeOffer/WhatWeOffer";
+import bgContactsSocratPage from "@/public/images/contact/bg-cont-socrat.jpg";
 
-import FAQ from '../../../components/FAQ/FAQ';
+import FAQ from "../../../components/FAQ/FAQ";
 
 const SocratPage = () => {
   return (
@@ -12,7 +12,6 @@ const SocratPage = () => {
       <HeroSocrat />
       <SocratInfo />
       <FAQ />
-      <WhatWeOffer />
       <Contact backgroundImage={bgContactsSocratPage.src} />
     </div>
   );
