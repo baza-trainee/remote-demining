0c0ae2ae053deab0d51cc327676f7867ef1eeee8
olexanderf <alex1990sfreem@gmail.com>
2023-08-31 19:12:00 +0300

    fix bug 70 and ref 63

--- app/client/contacts/page.tsx
+++ app/client/contacts/page.tsx
@@ -3,7 +3,6 @@ import ContactsPage from "@/components/ContactsPage/ContactsPage";
 export default function Contacts() {
   return (
     <main>
-      {/* <ScrollUp /> */}
       <ContactsPage />
     </main>
   );
