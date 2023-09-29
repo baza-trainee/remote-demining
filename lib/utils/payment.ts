c0df13012008687c6d62fbe1f4dadf26c3207f80
olexanderf <alex1990sfreem@gmail.com>
2023-09-29 19:00:27 +0300

    upd payment

--- lib/utils/payment.ts
+++ lib/utils/payment.ts
@@ -1,42 +1,58 @@
 import crypto from "crypto";
-import axios from "axios";
 
 interface OrderBodyType {
   merchantAccount: string;
   merchantDomainName: string;
   orderReference: string;
-  orderDate: string;
+  orderDate: number;
   amount: string;
   currency: "UAH";
-  productName: string;
-  productCount: string;
-  productPrice: string;
+  productName: [string];
+  productCount: [string];
+  productPrice: [string];
 }
 
-const sendToWFPay = async (orderBody: OrderBodyType, defaultPaymentSystem: string, signature: string) => {
+const sendToWFPay = async (
+  orderBody: OrderBodyType,
+  defaultPaymentSystem: string,
+  signature: string,
+) => {
   try {
-    const res = await axios.post("https://secure.wayforpay.com/pay", {
-      request: { ...orderBody, defaultPaymentSystem, signature },
-    });
-    await console.log(res);
+    // const res = await axios.post("https://secure.wayforpay.com/pay", {
+    //   transactionType: "CREATE_INVOICE",
+    //   ...orderBody,
+    //   apiVersion: 1,
+    //   defaultPaymentSystem,
+    //   signature,
+    // });
+    // console.log(signatureRaw)
+    console.log(JSON.stringify({
+      transactionType: "CREATE_INVOICE",
+      ...orderBody,
+      apiVersion: 1,
+      defaultPaymentSystem,
+      signature,
+    }))
+    // await console.log(res);
   } catch (error) {
-    throw (error);
+    throw error;
   }
 };
 
 const sendPayment = (amount: number, defaultPaymentSystem: string) => {
+
   const merchantSecretKey = "flk3409refn54t54t*FNJRET";
 
   const orderBody: OrderBodyType = {
     merchantAccount: "test_merch_n1",
-    merchantDomainName: "remote-demining.vercel.app",
+    merchantDomainName: "remote-demining-kappa.vercel.app",
     orderReference: Date.now().toString(),
-    orderDate: Date.now().toString(),
+    orderDate: Date.now(),
     amount: `${amount}`,
     currency: "UAH",
-    productName: "Remote Demining Support",
-    productCount: "1",
-    productPrice: `${amount}`,
+    productName: ["Remote Demining Support"],
+    productCount: ["1"],
+    productPrice: [`${amount}`],
   };
 
   const signatureRaw = Buffer.from(
@@ -44,8 +60,8 @@ const sendPayment = (amount: number, defaultPaymentSystem: string) => {
     "utf-8"
   ).toString();
   const signature = crypto
-    .createHmac("md5", signatureRaw)
-    .update(merchantSecretKey)
+    .createHmac("md5", merchantSecretKey)
+    .update(signatureRaw)
     .digest("hex");
 
   sendToWFPay(orderBody, defaultPaymentSystem, signature);
